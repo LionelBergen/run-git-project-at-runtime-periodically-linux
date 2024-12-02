b92e33b3c9b7944fb881c14ce9507f123e5a8459
dustytrash <Lionelbergen@live.com>
2024-12-01 21:08:49 -0600

    Append to logfile, dont replace

--- startup.sh
+++ startup.sh
@@ -8,15 +8,15 @@ PROJECT_2_DIRECTORY="../git/reddit-agree-with-you"
 
 # Runs a git pull for this directory
 if ["$auto_update_this_dir" = true]; then
-  git pull > $LOGFILE
+  git pull >> $LOGFILE
 fi
 
-forever stopall > $LOGFILE
+forever stopall >> $LOGFILE
 
-git -C $PROJECT_1_DIRECTORY pull > $LOGFILE
-git -C $PROJECT_2_DIRECTORY pull > $LOGFILE
+git -C $PROJECT_1_DIRECTORY pull >> $LOGFILE
+git -C $PROJECT_2_DIRECTORY pull >> $LOGFILE
 
-forever start $PROJECT_1_DIRECTORY -l $PROJECT_1_LOGFILE > $LOGFILE
-forever start $PROJECT_2_DIRECTORY -l $PROJECT_2_LOGFILE > $LOGFILE
+forever start $PROJECT_1_DIRECTORY -l $PROJECT_1_LOGFILE >> $LOGFILE
+forever start $PROJECT_2_DIRECTORY -l $PROJECT_2_LOGFILE >> $LOGFILE
 
-echo "Finished startup.sh" > $LOGFILE
+echo "Finished startup.sh" >> $LOGFILE
