6658f03945b310cc7bd74b56bfae7dc1bc82e45f
dustytrash <Lionelbergen@live.com>
2024-12-01 21:35:29 -0600

    fixed process log files

--- startup.sh
+++ startup.sh
@@ -6,6 +6,7 @@ PROJECT_1_LOGFILE=reddit_comment_reader.log
 PROJECT_2_LOGFILE=reddit_agree_with_you.log
 PROJECT_1_DIRECTORY="../git/reddit-comment-reader"
 PROJECT_2_DIRECTORY="../git/reddit-agree-with-you"
+START_PROJECT_FILE=/app.js
 
 # Runs a git pull for this directory
 if [ "$auto_update_this_dir" = true ] ; then
@@ -17,7 +18,7 @@ forever stopall >> $LOGFILE
 git -C $PROJECT_1_DIRECTORY pull >> $LOGFILE
 git -C $PROJECT_2_DIRECTORY pull >> $LOGFILE
 
-forever -c "node -r dotenv/config" --workingDir $PROJECT_1_DIRECTORY -l $cwd/$PROJECT_1_LOGFILE start $PROJECT_1_DIRECTORY/app.js >> $LOGFILE
-# forever start $PROJECT_2_DIRECTORY -l $cwd/$PROJECT_2_LOGFILE --workingDir $PROJECT_2_DIRECTORY >> $LOGFILE
+forever -c "node -r dotenv/config" --workingDir $PROJECT_1_DIRECTORY -l $cwd/$PROJECT_1_LOGFILE start $PROJECT_1_DIRECTORY$START_PROJECT_FILE >> $LOGFILE
+forever -c "node -r dotenv/config" --workingDir $PROJECT_2_DIRECTORY -l $cwd/$PROJECT_2_LOGFILE start $PROJECT_2_DIRECTORY$START_PROJECT_FILE >> $LOGFILE
 
 echo "Finished startup.sh" >> $LOGFILE
