67b9545b1963f099d5541fed83e3c18cfcbf1d48
dustytrash <Lionelbergen@live.com>
2024-12-01 20:21:04 -0600

    lots

--- README.md
+++ README.md
@@ -6,5 +6,7 @@ This way you can update your project via GIT and your machine should pickup thos
 
 
 # Quick Start   
+0) If running multiple programs, you should either duplicate this prosses or ensure your program has a way to run via commandline & as a background service    
+0.2) If it's a Node project, you can `forever` installed globally via `npm install forever -g`.  
 1) Create a crontab on your linux machine E.G `crontab -e`, put the file in the location of this directory, given the name `crontab`. I put mine in `/home/myusername/Desktop/startup/crontab`.  
 2) 
--- startup.sh
+++ startup.sh
@@ -1,7 +1,21 @@
 #!/bin/sh  
-LOGILE=log.txt
-PROJECT_DIRECTORY="../git/reddit-comment-reader"
+auto_update_this_dir=true
+LOGILE=startup.log
+PROJECT_1_LOGILE=reddit_comment_reader.log
+PROJECT_2_LOGILE=reddit_agree_with_you.log
+PROJECT_1_DIRECTORY="../git/reddit-comment-reader"
+PROJECT_2_DIRECTORY="../git/reddit-agree-with-you"
 
-npm run --prefix $PROJECT_DIRECTORY start
+# Runs a git pull for this directory
+if ["$auto_update_this_dir" = true]; then
+  git pull
+fi
 
-echo "Hello World" > log.txt
+forever stopall > $LOGFILE
+
+git -C $PROJECT_1_DIRECTORY pull > $LOGILE
+
+forever start $PROJECT_1_DIRECTORY > $PROJECT_1_LOGILE
+forever start $PROJECT_2_DIRECTORY > $PROJECT_1_LOGILE
+
+echo "Finished startup.sh" > $LOGILE
